e2d905204b541e84090a6634756eca83ea46b57b
smenaria2 <124450225+smenaria2@users.noreply.github.com>
2025-06-28 14:01:25 +0400

    Create draggable mini player widget and add song page review editing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import { AudioPlayerProvider } from "@/hooks/useAudioPlayer";
-import GlobalMiniPlayer from "@/components/GlobalMiniPlayer";
+import DraggableMiniPlayer from "@/components/DraggableMiniPlayer";
 import Index from "./pages/Index";
 import Submit from "./pages/Submit";
 import Profile from "./pages/Profile";
@@ -32,7 +31,7 @@ const App = () => (
               <Route path="/song/:id" element={<Song />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-            <GlobalMiniPlayer />
+            <DraggableMiniPlayer />
           </BrowserRouter>
         </TooltipProvider>
       </AudioPlayerProvider>
