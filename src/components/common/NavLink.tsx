fbf56fd02ac7a1ea3bc8439ed2d1f065bd5a711b
smenaria2 <124450225+smenaria2@users.noreply.github.com>
2025-06-29 15:37:46 +0400

    Updated NavLink.tsx

--- src/components/common/NavLink.tsx
+++ src/components/common/NavLink.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { LucideIcon } from "lucide-react";
+import { DivideIcon as LucideIcon } from "lucide-react";
 
 interface NavLinkProps {
   to: string;
